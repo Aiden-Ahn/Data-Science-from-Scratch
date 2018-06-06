eb4f8b9f7b4e691038d503c4a1da7b5bf51117a0
proghjy <proghjy@gmail.com>
2018-06-06 16:15:28 +0900

    #5 백터들의 합

--- linearAlgebral.py
+++ linearAlgebral.py
@@ -18,9 +18,15 @@ def vector_subtract(v, w):
     """subtracts two vectors componentwise"""
     return [v_i - w_i for v_i, w_i in zip(v,w)]
 
+def vector_sum(vectors):
+    """sums all corresponding elements"""
+    return reduce(vector_add, vectors)
+
 if __name__ == "__main__":
-    v = [2, 1]
-    w = [math.sqrt(.25), math.sqrt(.75)]
+    v = [x for x in range(1, 11,2)]
+    w = [y for y in range(11, 21,2)]
     print(v)
     print(w)
-    print(vector_subtract(v, w))
+    vectors = [v,w,v,w,v,w]
+    print(vectors)
+    print(vector_sum(vectors))
