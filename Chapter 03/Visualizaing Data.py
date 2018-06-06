4cfa7a61f5c895e79a081aa50c6482e03e50ffd2
proghjy <proghjy@gmail.com>
2018-06-06 13:51:02 +0900

    #1 간단한 바 차트

--- Chapter 03/Visualizaing Data.py
+++ Chapter 03/Visualizaing Data.py
@@ -17,6 +17,27 @@ def make_simple_line_chart():
     plt.ylabel("Billions of $")
     plt.show()
 
+def make_simple_bar_chart():
+    """ 그림 3-2. 간단한 막대 그래프 """
+
+    movies = ["Annie Hall", "Ben-Hur", "Casablanca", "Gandhi", "West Side Story"]
+    num_oscars = [5, 11, 3, 8, 10]
+
+    # bars are by default width 0.8, so we'll add 0.1 to the left coordinates
+    # so that each bar is centered
+    xs = [i + 0.1 for i, _ in enumerate(movies)]
+
+    # plot bars with left x-coordinates [xs], heights [num_oscars]
+    plt.bar(xs, num_oscars)
+    plt.ylabel("# of Academy Awards")
+    plt.title("My Favorite Movies")
+
+    # label x-axis with movie names at bar centers
+    plt.xticks([i + 0.5 for i, _ in enumerate(movies)], movies)
+
+    plt.show()
+
 if __name__ == "__main__":
     
     make_simple_line_chart()
+    make_simple_bar_chart()
