fad62c0915802d11a9f3f6320649c3dddfb3acf1
proghjy <proghjy@gmail.com>
2018-06-06 16:49:02 +0900

    #5 벡터들의 합 처리시간 측정

--- linearAlgebral.py
+++ linearAlgebral.py
@@ -2,9 +2,11 @@
 
 from __future__ import division # want 3 / 2 == 1.5
 import re, math, random # regexes, math functions, random numbers
-import matplotlib.pyplot as plt # pyplot
+import matplotlib.pyplot as plt # pyplotimport numpy as np
 from collections import defaultdict, Counter
 from functools import partial
+
+import numpy as np
 
 # 
 # functions for working with vectors
@@ -22,11 +24,18 @@ def vector_sum(vectors):
     """sums all corresponding elements"""
     return reduce(vector_add, vectors)
 
+def runtime(f):
+    from datetime import datetime 
+    start_time = datetime.now() 
+    data = f
+    time_elapsed = datetime.now() - start_time 
+    print('{}'.format(time_elapsed), data)
+
 if __name__ == "__main__":
     v = [x for x in range(1, 11,2)]
     w = [y for y in range(11, 21,2)]
     print(v)
     print(w)
     vectors = [v,w,v,w,v,w]
-    print(vectors)
-    print(vector_sum(vectors))
+    runtime(vector_sum(vectors))
+    runtime(np.sum([v,w,v,w,v,w], axis=0))
